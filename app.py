766160be5af582ad03e4a27a1777eadaeb42b9c0
DanielaDeRamos2 <deramosdaniela737@gmail.com>
2025-10-21 17:27:45 -0300

    front do crud noticias

--- app.py
+++ app.py
@@ -6,5 +6,18 @@ app = Flask(__name__)
 def home():
     return render_template('home.html')
 
+@app.route('/noticias')
+def noticias():
+    return render_template('noticias.html')
+
+@app.route('/criar-noticia')
+def criarNoticias():
+    return render_template('criar-noticia.html')
+
+@app.route('/listar-noticias')
+def listarNoticias():
+    return render_template('listar-noticias.html')
+    
+
 if __name__ == '__main__':
     app.run(debug=True)
